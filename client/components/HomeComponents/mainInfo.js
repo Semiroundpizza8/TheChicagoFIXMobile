6d597457212dc4668befc376656149f1c4bf9772
Benjamin Odisho <Semiroundpizza8@gmail.com>
2018-06-19 19:40:29 -0400

    feat: Added services to MainInfo component

--- client/components/HomeComponents/mainInfo.js
+++ client/components/HomeComponents/mainInfo.js
@@ -11,28 +11,44 @@ const blackTextboxContainers = {
   backgroundColor: '#0e121c',
   color: 'white',
   height: '60vh',
-  width: '100vw'
+  width: '50vw'
 };
 
 export const mainInfo = () => (
-  <div style={blackTextboxContainers} >
-    <h1>Using a proactive approach to sports medicine to prevent injuries and keep employees performing at their best.</h1>
-    <p>At the Chicago FIX Mobile we bring the same treatment that professional athletes use to stay at the top of their game right to your jobsite to keep your employees performing at the top of theirs.</p>
-    <div style={{ listStyle: 'none', display: 'flex', flexWrap: 'wrap', justifyContent: 'center', flexDirection: 'column', marginTop: '2em' }}>
-      <div style={{ display: 'flex', justifyContent: 'space-around', marginLeft: "5%", marginRight: "5%", color: '#f15a29' }}>
-        <h4><Glyphicon glyph="arrow-down" /> EMR</h4>
-        <h4><Glyphicon glyph="arrow-down" /> OSHA Rates</h4>
-        <h4><Glyphicon glyph="arrow-down" /> Work Comp Premiums</h4>
-        <h4><Glyphicon glyph="arrow-down" /> Cost Work</h4>
+  <div style={{ display: 'flex' }}>
+    <div style={blackTextboxContainers} >
+      <h1>Using a proactive approach to sports medicine to prevent injuries and keep employees performing at their best.</h1>
+      <p>At the Chicago FIX Mobile we bring the same treatment that professional athletes use to stay at the top of their game right to your jobsite to keep your employees performing at the top of theirs.</p>
+      <div style={{ listStyle: 'none', display: 'flex', flexWrap: 'wrap', justifyContent: 'center', flexDirection: 'column', marginTop: '2em' }}>
+        <div style={{ display: 'flex', justifyContent: 'space-around', marginLeft: "5%", marginRight: "5%", color: '#f15a29' }}>
+          <h4><Glyphicon glyph="arrow-down" /> EMR</h4>
+          <h4><Glyphicon glyph="arrow-down" /> OSHA Rates</h4>
+          <h4><Glyphicon glyph="arrow-down" /> Work Comp Premiums</h4>
+          <h4><Glyphicon glyph="arrow-down" /> Cost Work</h4>
+        </div>
+        <div style={{ display: 'flex', justifyContent: 'space-around', marginLeft: "5%", marginRight: "5%", marginBottom: '2.5%', color: "#f9931e" }}>
+          <h4><Glyphicon glyph="arrow-up" /> Work Site Safety</h4>
+          <h4><Glyphicon glyph="arrow-up" /> Employee Wellness</h4>
+          <h4><Glyphicon glyph="arrow-up" /> Increase Productivity</h4>
+          <h4><Glyphicon glyph="arrow-up" /> Open New Jobs</h4>
+        </div>
       </div>
-      <div style={{ display: 'flex', justifyContent: 'space-around', marginLeft: "5%", marginRight: "5%", marginBottom: '2.5%', color: "#f9931e" }}>
-        <h4><Glyphicon glyph="arrow-up" /> Work Site Safety</h4>
-        <h4><Glyphicon glyph="arrow-up" /> Employee Wellness</h4>
-        <h4><Glyphicon glyph="arrow-up" /> Increase Productivity</h4>
-        <h4><Glyphicon glyph="arrow-up" /> Open New Jobs</h4>
-      </div>
+    </div >
+    <div style={blackTextboxContainers}>
+      <h1>At The Chicago FIX Mobile, we offer:</h1>
+      <ul>
+        <li>Chiropractic Care</li>
+        <li>Compression Ice Therapy</li>
+        <li>Thermotherapy</li>
+        <li>Assisted Stretching</li>
+        <li>Personal Training</li>
+        <li>Wellness Services</li>
+        <li>Injury Evaluation</li>
+        <li>Rehabilitation</li>
+
+      </ul>
     </div>
-  </div >
+  </div>
 )
 
 export default connect(null, null)(mainInfo);
